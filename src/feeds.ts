8f2ddf499d16e506d0b2120216eadcf4717d7d77
Victor Jean <victorjean@pm.me>
2026-03-21 17:26:55 +0800

    chore: update RSS feed URLs (maintenance)
    - Fix protocol from http to https for washingtonpost feed
    - Update Google News search feed URLs with proper formatting
    - Correct Le Monde and El País feed URLs
    - Update All Africa RSS feed URL
    
    These changes improve feed compatibility and reliability

--- src/feeds.ts
+++ src/feeds.ts
@@ -5,31 +5,31 @@ export const feedUrls = [
     "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
     "https://www.aljazeera.com/xml/rss/all.xml",
     "https://www.theguardian.com/world/rss",
-    "http://feeds.washingtonpost.com/rss/world",
+    "https://feeds.washingtonpost.com/rss/world",
     "https://www.npr.org/rss/rss.php?id=1001",
     "http://feeds.foxnews.com/foxnews/latest",
     "https://feeds.a.dj.com/rss/RSSWorldNews.xml", // WSJ
-    "https://www.usatoday.com/rss/news",
+    "https://news.google.com/rss/search?q=when:24h+source:USA_Today&hl=en-US&gl=US&ceid=US:en",
+    "https://news.google.com/rss/search?q=when:24h+source:Reuters&hl=en-US&gl=US&ceid=US:en",
     "https://news.google.com/rss", // Google News Top Stories
-    "https://www.reutersagency.com/feed/",
-    "https://www.lemonde.fr/en/rss/tout_le_monde.xml", // Le Monde English
+    "https://www.lemonde.fr/en/rss/une.xml", // Le Monde English (Corrected)
     "https://www.france24.com/en/rss",
     "https://rss.dw.com/rdf/rss-en-all",
     "https://www.euronews.com/rss?format=mrss&level=vertical&name=news",
-    "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/english/portada",
+    "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada/english",
     "https://www.rte.ie/news/rss/news-headlines.xml",
 
     // Africa
-    "https://allafrica.com/tools/headlines/rss/front/main.xml",
+    "https://allafrica.com/static/rss/main.xml",
     "https://www.africanews.com/feed/",
     "https://www.mg.co.za/feed/", // Mail & Guardian (SA)
-    "http://feeds.news24.com/articles/news24/TopStories/rss",
+    "https://www.news24.com/rss/topstories",
     "https://www.premiumtimesng.com/feed",
     "https://www.theeastafrican.co.ke/service/rss/2522/2522/view/asfeed/1709424/index.xml",
     "https://www.moroccoworldnews.com/feed",
 
     // Asia
-    "https://www.scmp.com/rss/2/feed.xml", // South China Morning Post
+    "https://www.scmp.com/rss/91/feed", // South China Morning Post (World)
     "https://www.japantimes.co.jp/feed/",
     "https://www.channelnewsasia.com/rssfeeds/8395986", // CNA
     "https://www.thehindu.com/news/national/feeder/default.rss",
@@ -39,7 +39,7 @@ export const feedUrls = [
     "https://www.bangkokpost.com/rss/data/topstories.xml",
 
     // South America / LatAm
-    "https://www.buenosairestimes.com.ar/rss",
+    "https://www.perfil.com/rss/batimes", // Buenos Aires Times proxy
     "https://www.riotimesonline.com/feed/",
     "https://en.mercopress.com/rss/",
     "https://buenosairesherald.com/feed",
@@ -47,20 +47,20 @@ export const feedUrls = [
 
     // Middle East
     "https://www.haaretz.com/cmlink/1.4621172",
-    "https://www.jpost.com/rss/rssfeedsmain.aspx",
-    "https://www.khaleejtimes.com/rss/home",
+    "https://rss.jpost.com/rss/rssfeedsfrontpage.aspx",
+    "https://www.khaleejtimes.com/section/uae/rss",
 
     // UK
     "http://feeds.skynews.com/feeds/rss/home.xml",
     "https://www.independent.co.uk/news/rss",
-    "https://www.telegraph.co.uk/news/rss.xml",
+    "https://www.telegraph.co.uk/rss.xml",
 
     // Canada
     "https://www.cbc.ca/cmlink/rss-topstories",
     "https://globalnews.ca/feed/",
 
     // Australia
-    "https://www.abc.net.au/news/feed/45910-rss.xml",
+    "https://www.abc.net.au/news/feed/46182/rss.xml", // Top Stories
     "https://www.smh.com.au/rss/feed.xml",
 
     // Tech
@@ -72,8 +72,8 @@ export const feedUrls = [
 
     // Business / Finance
     "https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "https://www.economist.com/sections/business-finance/rss.xml",
-    "https://www.ft.com/rss/home", // Might require subscription logic, checking if open
+    "https://www.economist.com/business/rss.xml",
+    "https://www.ft.com/rss/home", 
 
     // Sports / Entertainment
     "https://www.espn.com/espn/rss/news",
@@ -81,7 +81,7 @@ export const feedUrls = [
     "https://variety.com/feed/",
 
     // Science
-    "https://www.sciencedaily.com/rss/top_news.xml",
+    "https://www.sciencedaily.com/rss/all.xml",
     "https://www.nasa.gov/rss/dyn/breaking_news.rss",
     "https://www.newscientist.com/feed/home/",
 
